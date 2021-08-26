7f805bbf2da04a8dae3d1d60d3c7d4ad64fd99be
Giovanni Buroni <32134829+giobbu@users.noreply.github.com>
2021-08-26 13:21:38 +0200

    add fct layer deck

--- testing/test_module.py
+++ testing/test_module.py
@@ -162,7 +162,7 @@ def testing(out_sqc, lst, streets, timestamp, X_vl, X_ts, Y_ts):
             time.sleep(2)
 
             
-            
+            del layer, r
             del recent_rmse_ci, recent_rmse_dot
             del recent_mae_ci, recent_mae_dot
             del line_past, line_targ, line_pred, line_zoom
